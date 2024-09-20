9a231856b1a59f295fa66e509c9eeef270077938
prashantforsure <prashantforsure@gmail.com>
2024-09-20 07:08:05 +0530

    auth added

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import type { Session, User } from 'next-auth'
+import type { JWT } from 'next-auth/jwt'
+
+type UserId = string
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: UserId
+    email?: string | null
+  }
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: User & {
+      id: UserId
+      email?: string | null
+      image?: string | null
+    }
+  }
+}
