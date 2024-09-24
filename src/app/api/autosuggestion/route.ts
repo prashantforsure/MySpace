b341c727501bbed6954d6cede13d06b899c259eb
prashantforsure <prashantforsure@gmail.com>
2024-09-24 09:38:20 +0530

    booking page

---
+++ src/app/api/autosuggestion/route.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+export async function POST(req: NextApiRequest){
+
+}
